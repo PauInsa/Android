1a3d70297ba30f419bacc0f857c85f37eb081b65
Pau Insa Fajardo <pau.insa@enti.cat>
2023-09-26 10:05:30 +0200

    Media clase

--- app/src/main/java/com/example/andoridclase/MainActivity.kt
+++ app/src/main/java/com/example/andoridclase/MainActivity.kt
@@ -13,10 +13,48 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.andoridclase.ui.theme.AndoridClaseTheme
 
 class MainActivity : ComponentActivity() {
+    
+    open class Fruit(val name: String = "Fruta Generica")
+    {
+        
+    }
+    
+    class Apple(): Fruit("Manzana")
+    {
+        fun AppleFunc(){
+            
+        }
+    }
+
+    class Banana(): Fruit("Banana")
+    {
+        fun BananaFunc(){
+
+        }
+    }
+
+    enum class Colors(val color: Color, val translationKey: String)
+    {
+        Red(Color.Red, "Color_Key_Red"),
+        Green(Color.Green, "Color_Key_Green"),
+        Blue(Color.Blue, "Color_Key_Blue"),
+        Yellow(Color.Yellow, "Color_Key_Yellow")
+    }
+
+    enum class Operations(val operation: (Int, Int) -> Int, val priority: Int)
+    {
+        Add({a, b -> a + b}, 0),
+        Subtract({a, b-> a - b}, 0);
+
+        fun Calculate(a: Int, b: Int) : Int {
+            return this.operation(a,b)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -24,18 +62,61 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
 
-                    Column(){
-                        PrintToScreen("hola")
-                        PrintToScreen("que tal")
+                    Column() {
+                        
+                        val fruits = listOf(Banana(), Apple(), null)
+                        
+                        fruits.forEach{ fruit ->
+                            when(fruit) {
+                                is Banana -> {
+                                    fruit.BananaFunc()
+                                }
+                                is Apple -> {
+                                    
+                                }
+                                else -> PrintToScreen(name = "Kesesto")
+                            }
+                            
+                        }
 
-                        Spacer(modifier = Modifier.height(16.dp))
+                        val weekDay = "Dilluns"
 
-                        val names = listOf("armario", "rigoberto", "cogombre")
+                        when(weekDay) {
+                            "Lunes" -> "Es lunes"
+                            "Martes" -> "Es martes"
+                        }
+
+                        /*val operation = Operations.Subtract
+
+                        val result = operation.Calculate(3,2)
+                        
+                        PrintToScreen(name = result.toString())*/
+
+                        /*val names = listOf("armario", "rigoberto", "cogombre")
 
                         for(name in names)
                         {
                             PrintToScreen(name)
                         }
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        for(i in names.indices)
+                        {
+                            PrintToScreen(i.toString())
+                        }
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        names.forEach {
+                            name -> PrintToScreen(name)
+                        }
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        names.forEachIndexed {
+                            i, name -> PrintToScreen(i.toString())
+                        }*/
                     }
                 }
             }
@@ -44,10 +125,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun PrintToScreen(name: String, modifier: Modifier = Modifier) {
+fun PrintToScreen(name: String, modifier: Modifier = Modifier, color:Color? = null) {
 
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        color = color ?: Color.Black
     )
 }
